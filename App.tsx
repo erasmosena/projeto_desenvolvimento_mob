4484f5f5e7e4e6a91a87645c7fd3443cfa556937
Erasmo Sena <erasmo.sena@gmail.com>
2022-10-07 20:38:19 -0300

    commit inicial

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import AdicionarAluno from './src/components/aluno/adicionar-aluno';
+import Formulario from './src/components/Formulario';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import ListarAlunos from './src/components/aluno/listar-aluno';
+import DatabaseInit from './src/database/database-init';
+
+export default class App extends React.Component { 
+
+
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    new DatabaseInit
+    console.log("initialize database")
+  }
+
+
+  render() {
+    const Drawer = createDrawerNavigator();
+    return (
+      <NavigationContainer>
+      <Drawer.Navigator  >
+        <Drawer.Screen name="Ver Eventos" component={ListarAlunos} />
+        <Drawer.Screen name="Adicionar Novo Evento" component={AdicionarAluno} />
+      </Drawer.Navigator>     
+    </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +38,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: Dimensions.get("screen").height
   },
 });
